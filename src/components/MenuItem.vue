680259aaa61f91a229d7e74d1b9bb932c1f6a01a
EduardoVitorInocencio <eduardovitor16@gmail.com>
2025-07-06 16:50:08 -0300

    feat: Criação do componente MenuItem

--- src/components/MenuItem.vue
+++ src/components/MenuItem.vue
@@ -2,54 +2,63 @@
 import { ref, toRefs, watchEffect } from 'vue';
 import { useRoute } from 'vue-router';
 
-const route = useRoute()
+const route = useRoute();
 
 const props = defineProps({
-    iconString: String,
-    iconSize: Number,
-    pageUrl: String,
-    name: String
-})
-const { iconString, pageUrl, name, iconSize } = toRefs(props)
+  iconString: String,
+  iconSize: Number,
+  pageUrl: String,
+  name: String
+});
+const { iconString, pageUrl, name, iconSize } = toRefs(props);
 
-let icon = ref(null)
-let textIsHover = ref(false)
+const icon = ref(null);
+const textIsHover = ref(false);
 
 watchEffect(() => {
-    if (route.path == pageUrl.value) {
-        icon.value = iconString.value + '-active'
-        textIsHover.value = true
-    } else {
-        icon.value = iconString.value + '-inactive'
-        textIsHover.value = false
-    }
-})
+  if (route.path === pageUrl.value) {
+    icon.value = `${iconString.value}-active`;
+    textIsHover.value = true;
+  } else {
+    icon.value = `${iconString.value}-inactive`;
+    textIsHover.value = false;
+  }
+});
 
 const isHover = () => {
-    if (icon.value === iconString.value + '-active') return
+  if (icon.value === `${iconString.value}-active`) return;
 
-    if (icon.value === iconString.value + '-inactive') {
-        icon.value = iconString.value + '-inactive-hover'
-        textIsHover.value = true
-    } else if (icon.value === iconString.value + '-inactive-hover') {
-        icon.value = iconString.value + '-inactive'
-        textIsHover.value = false
-    }
-}
+  if (icon.value === `${iconString.value}-inactive`) {
+    icon.value = `${iconString.value}-inactive-hover`;
+    textIsHover.value = true;
+  } else if (icon.value === `${iconString.value}-inactive-hover`) {
+    icon.value = `${iconString.value}-inactive`;
+    textIsHover.value = false;
+  }
+};
 </script>
 
 <template>
-    <li 
-        class="flex items-center justify-start pb-4 cursor-pointer" 
-        @mouseenter="isHover()" 
-        @mouseleave="isHover()"
+  <li 
+    class="flex items-center justify-start pb-4 cursor-pointer" 
+    @mouseenter="isHover" 
+    @mouseleave="isHover"
+  >
+    <img
+      v-if="icon"
+      :width="iconSize"
+      :src="`/images/icons/${icon}.png`"
+      alt="icon"
+      @error="console.warn(`Imagem não encontrada: /images/icons/${icon}.png`)"
+    />
+    
+    <div
+      :class="[
+        'font-semibold text-[14px] ml-4 mt-0.5',
+        textIsHover ? 'text-white' : 'text-gray-400'
+      ]"
     >
-        <img :width="iconSize" :src="`/images/icons/${icon}.png`">
-        <div
-            :class="textIsHover ? 'text-white ' : 'text-gray-400'"
-            class="font-semibold text-[14px] ml-4 mt-0.5"
-        >
-            <span :class="route.path == pageUrl ? 'text-white' : ''">{{ name }}</span>
-        </div>
-    </li>
+      <span :class="{ 'text-white': route.path === pageUrl }">{{ name }}</span>
+    </div>
+  </li>
 </template>
